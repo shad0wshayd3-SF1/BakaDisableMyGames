a82e114df155b90536c7b98eed62c2fdb0a28cdb
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2023-09-09 19:33:36 -0600

    formatting, added startup check, disabled path

--- Plugin/src/main.cpp
+++ Plugin/src/main.cpp
@@ -3,24 +3,31 @@ class Hooks
 public:
 	static void Install()
 	{
-		hkFormatString<0x01FC0880, 0x082>::Install();
-		hkFormatString<0x0218CF50, 0x31E>::Install();
-		hkFormatString<0x0218E5F0, 0x29F>::Install();
-		hkFormatString<0x0218F59C, 0x023>::Install();
+		hkPhotoModePath<0x01FC0880, 0x082>::Install();
+		hkPhotoModePath<0x0218CF50, 0x31E>::Install();
+		hkPhotoModePath<0x0218E5F0, 0x29F>::Install();
+		hkPhotoModePath<0x0218F59C, 0x023>::Install();
 
-		hkMOTDPatch<0x02073140, 0x2C1>::Install();
-		hkMOTDPatch<0x02073AC0, 0x14B>::Install();
+		hkMessageOfTheDayPath<0x02073140, 0x2C1>::Install();
+		hkMessageOfTheDayPath<0x02073AC0, 0x14B>::Install();
+
+		hkDisableLooseFileLocation<0x034BAAC0, 0x172>::Install();
 	}
 
-	static void GetPath()
+	static void SetPath()
 	{
-		std::string temp;
-		temp.resize(260);
-		GetCurrentDirectoryA(temp.size(), temp.data());
+		std::string path;
+		path.resize(260);
+		GetCurrentDirectoryA(path.size(), path.data());
 
 		detail::path.clear();
-		detail::path.append(temp.data());
+		detail::path.append(path.data());
 		detail::path.append("\\");
+	}
+
+	static std::string& GetPath()
+	{
+		return detail::path;
 	}
 
 private:
@@ -31,50 +38,71 @@ private:
 	};
 
 	template <std::uintptr_t ADDR, std::ptrdiff_t OFF>
-	class hkFormatString
+	class hkPhotoModePath
 	{
 	public:
 		static void Install()
 		{
 			static REL::Relocation<std::uintptr_t> target{ REL::Offset(ADDR), OFF };
 			auto& trampoline = SFSE::GetTrampoline();
-			_FormatString = trampoline.write_call<5>(target.address(), FormatString);
+			_PhotoModePath = trampoline.write_call<5>(target.address(), PhotoModePath);
 		}
 
 	private:
-		static void FormatString(char* a_buffer, std::size_t a_size, char* a_format, char* a_arg1, char* a_arg2)
+		static void PhotoModePath(
+			[[maybe_unused]] char* a_destination,
+			[[maybe_unused]] std::size_t a_size,
+			[[maybe_unused]] const char* a_source,
+			[[maybe_unused]] const char* a_vArg1,
+			[[maybe_unused]] const char* a_vArg2)
 		{
-			return _FormatString(
-				a_buffer,
-				a_size,
-				a_format,
-				detail::path.data(),
-				a_arg2);
+			return _PhotoModePath(a_destination, a_size, a_source, detail::path.data(), a_vArg2);
 		}
 
-		inline static REL::Relocation<decltype(&FormatString)> _FormatString;
+		inline static REL::Relocation<decltype(&PhotoModePath)> _PhotoModePath;
 	};
 
 	template <std::uintptr_t ADDR, std::ptrdiff_t OFF>
-	class hkMOTDPatch
+	class hkMessageOfTheDayPath
 	{
 	public:
 		static void Install()
 		{
 			static REL::Relocation<std::uintptr_t> target{ REL::Offset(ADDR), OFF };
 			auto& trampoline = SFSE::GetTrampoline();
-			_FormatString = trampoline.write_call<5>(target.address(), FormatString);
+			trampoline.write_call<5>(target.address(), MessageOfTheDayPath);
 		}
 
 	private:
-		static void FormatString(char* a_buffer)
+		static void MessageOfTheDayPath(
+			[[maybe_unused]] char* a_destination)
 		{
-			std::string data{ detail::path.data() };
-			data.append("Data\\Textures\\MOTD_Media\\"sv);
-			a_buffer = data.data();
+			auto path = std::filesystem::path{ detail::path };
+			path /= "Data/Textures/MOTD_Media/"sv;
+			strcpy_s(a_destination, 260, path.make_preferred().string().data());
+		}
+	};
+
+	template <std::uintptr_t ADDR, std::ptrdiff_t OFF>
+	class hkDisableLooseFileLocation
+	{
+	public:
+		static void Install()
+		{
+			static REL::Relocation<std::uintptr_t> target{ REL::Offset(ADDR), OFF };
+			auto& trampoline = SFSE::GetTrampoline();
+			trampoline.write_call<5>(target.address(), DisableLooseFileLocation);
 		}
 
-		inline static REL::Relocation<decltype(&FormatString)> _FormatString;
+	private:
+		static errno_t DisableLooseFileLocation(
+			[[maybe_unused]] char* a_destination,
+			[[maybe_unused]] std::size_t a_size,
+			[[maybe_unused]] const char* a_source)
+		{
+			auto path = std::filesystem::path{ detail::path };
+			return strcpy_s(a_destination, a_size, path.make_preferred().string().data());
+		}
 	};
 };
 
@@ -109,6 +137,44 @@ namespace
 	}
 }
 
+DLLEXPORT void SFSEAPI SFSEPlugin_Preload(const SFSE::LoadInterface* a_sfse)
+{
+	SFSE::Init(a_sfse);
+
+	DKUtil::Logger::Init(Plugin::NAME, std::to_string(Plugin::Version));
+
+	INFO("{} v{} preloaded"sv, Plugin::NAME, Plugin::Version);
+
+	Hooks::SetPath();
+
+	PWSTR folderPath{ nullptr };
+	if (SHGetKnownFolderPath(FOLDERID_Documents, KNOWN_FOLDER_FLAG::KF_FLAG_DEFAULT, NULL, &folderPath) == S_OK)
+	{
+		auto path = std::filesystem::path{ folderPath };
+		path /= "My Games/Starfield/Data/"sv;
+
+		if (std::filesystem::exists(path))
+		{
+			auto data = std::filesystem::path{ Hooks::GetPath() };
+			data /= "Data"sv;
+
+			static constexpr auto MessageText = std::string_view{
+				"\n\nA Data folder has been detected in Starfield's \"My Games\" folder."sv
+				"\n\nWith this mod installed, the Data folder in Starfield's \"My Games\" folder is disabled. Files located there will not load, including Photo Mode photos shown in the Photo Gallery and during Load Screens."sv
+				"\n\nThe path to this folder is:\n{}\n\nCopy any files contained in that Data folder to the game's Data folder, then delete the Data folder in \"My Games\"."sv
+				"\n\nThe path to the game's Data folder is: \n{}"sv
+				"\n\nWhen you have successfully done this, this message will stop appearing, and you will be able to launch the game."sv
+			};
+
+			auto msg = fmt::format(
+				MessageText,
+				path.make_preferred().string(),
+				data.make_preferred().string());
+			REL::stl::report_and_fail(msg);
+		}
+	}
+}
+
 DLLEXPORT bool SFSEAPI SFSEPlugin_Load(const SFSE::LoadInterface* a_sfse)
 {
 #ifndef NDEBUG
@@ -117,16 +183,10 @@ DLLEXPORT bool SFSEAPI SFSEPlugin_Load(const SFSE::LoadInterface* a_sfse)
 	}
 #endif
 
-	SFSE::Init(a_sfse);
-
-	DKUtil::Logger::Init(Plugin::NAME, std::to_string(Plugin::Version));
-
-	INFO("{} v{} loaded", Plugin::NAME, Plugin::Version);
+	INFO("{} v{} loaded"sv, Plugin::NAME, Plugin::Version);
 
 	SFSE::AllocTrampoline(1 << 10);
 	SFSE::GetMessagingInterface()->RegisterListener(MessageCallback);
-
-	Hooks::GetPath();
 
 	return true;
 }
