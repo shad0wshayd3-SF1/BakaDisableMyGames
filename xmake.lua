3c35eace58e82fe51126d8c59b52eb98c077cfed
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2024-11-14 18:30:38 -0700

    fix: xmake recurse source

--- xmake.lua
+++ xmake.lua
@@ -38,8 +38,8 @@ target("BakaDisableMyGames")
     })
 
     -- add source files
-    add_files("src/*.cpp")
-    add_headerfiles("src/*.h")
+    add_files("src/**.cpp")
+    add_headerfiles("src/**.h")
     add_includedirs("src")
     set_pcxxheader("src/PCH.h")
 
